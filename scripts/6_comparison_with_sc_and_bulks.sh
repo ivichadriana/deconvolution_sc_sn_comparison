98f1dcc1da81bb5ac1b2b702c973c1bbd6c957c2
Adriana Ivich <aivich@xsede.org@c3cpu-a5-u1-4.rc.int.colorado.edu>
2025-05-19 14:39:37 -0600

    new notebooks, new script, new shell

---
+++ scripts/6_comparison_with_sc_and_bulks.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#SBATCH --job-name=RealBulk_andSC_Comparison
+#SBATCH --account=amc-general
+#SBATCH --output=output_RealBulk_andSC_Comparison_%A_%a.log
+#SBATCH --error=error_RealBulk_andSC_Comparison_%A_%a.log 
+#SBATCH --qos=normal
+#SBATCH --time=24:00:00                              
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+
+# Activate the conda environment
+echo "****** Activating environment... ******"
+source ~/.bashrc
+conda deactivate
+conda activate env_deconv
+
+sleep 5
+
+echo "****** Running dataset: ${res_name} ******"
+output_path="${output_root}/${res_name}"
+mkdir -p "$output_path"
+
+echo "****** Running prepare_real_bulk_clustering.py ******"
+python "${BASE_DIR}/scripts/prepare_real_bulk_clustering.py" \
+
+# Define relative data and output paths
+nb_path="${BASE_DIR}/notebooks"
+
+##########################################################################################################
+##########################                NOTEBOOK              #########################################
+
+input_notebook=${nb_path}/results_bulks_comparison.ipynb
+output_notebook=${nb_path}/results_bulks_comparison.ipynb
+
+##########################               EXECUTION             #########################################
+
+echo "****** Running notebook of bulk similarity: ******"
+
+papermill "$input_notebook" "$output_notebook"
+##########################################################################################################
+
+##########################################################################################################
+##########################                NOTEBOOK              #########################################
+
+input_notebook=${nb_path}/results_sc_comparison.ipynb
+output_notebook=${nb_path}/results_sc_comparison.ipynb
+
+##########################               EXECUTION             #########################################
+
+echo "****** Running notebookof sc similarity: ******"
+
+papermill "$input_notebook" "$output_notebook"
+##########################################################################################################
+
+# Deactivate the environment
+conda deactivate
